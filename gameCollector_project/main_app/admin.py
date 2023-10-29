02216586d72cfe5fbe18fb2209766196aab24614
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-10-28 21:42:49 -0700

    Registered models on the admin site, and added a create feature link to the navbar

--- gameCollector_project/main_app/admin.py
+++ gameCollector_project/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Features, Game, NewContent, Photo
 # Register your models here.
+admin.site.register(Features)
+admin.site.register(Game)
+admin.site.register(NewContent)
+admin.site.register(Photo)
